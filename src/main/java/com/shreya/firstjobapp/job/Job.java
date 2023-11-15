d0c1ee1046e60883834c250fffc8d48cead55771
Shreya Sharma <shreyashobha0201@gmail.com>
2023-11-15 12:37:01 +0530

    H2 connection done

--- src/main/java/com/shreya/firstjobapp/job/Job.java
+++ src/main/java/com/shreya/firstjobapp/job/Job.java
@@ -1,12 +1,22 @@
 package com.shreya.firstjobapp.job;
 
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name = "job_table")
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // manage primary key
     private Long id;
     private String title;
     private String description;
     private String minSalary;
     private String maxSalary;
     private String location;
+
+    public Job() {
+    }
+
     public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
         this.id = id;
         this.title = title;
--- src/main/java/com/shreya/firstjobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/shreya/firstjobapp/job/impl/JobServiceImpl.java
@@ -1,24 +1,81 @@
 package com.shreya.firstjobapp.job.impl;
 
 import com.shreya.firstjobapp.job.Job;
+import com.shreya.firstjobapp.job.JobRepository;
 import com.shreya.firstjobapp.job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private List<Job> jobs = new ArrayList<>();
-    private Long nextId = 1L;
+//    private List<Job> jobs = new ArrayList<>();
+    JobRepository jobRepository;
+//    private Long nextId = 1L;
+// autowiring
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
     @Override
     public List<Job> findAll() {
-        return jobs;
+        return jobRepository.findAll();
     }
 
     @Override
     public void createJob(Job job) {
-        job.setId(nextId++);
-        jobs.add(job);
+//        job.setId(nextId++);
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+//        for(Job job: jobs){
+//            if(job.getId().equals(id)){
+//                return job;
+//            }
+//        }
+//        return null;
+        return jobRepository.findById(id).orElse(null);
+    }
+//    https://www.geeksforgeeks.org/iterators-in-java/
+    @Override
+    public boolean deleteJobById(Long id) {
+//        Iterator<Job> iterator = jobs.iterator();
+//        while(iterator.hasNext()){
+//            Job job = iterator.next();
+//            if(job.getId().equals(id)){
+//                iterator.remove();
+//                return true;
+//            }
+//        }
+//        return false;
+        try{
+            jobRepository.deleteById(id);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+// An optional value is a way to handle situations where a value may or may not be present.
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+            if(jobOptional.isPresent()){
+                Job job = jobOptional.get();
+                job.setTitle(updatedJob.getTitle());
+                job.setDescription(updatedJob.getDescription());
+                job.setMinSalary(updatedJob.getMinSalary());
+                job.setMaxSalary(updatedJob.getMaxSalary());
+                job.setLocation(updatedJob.getLocation());
+                jobRepository.save(job);
+                return true;
+            }
+
+        return false;
     }
 }
